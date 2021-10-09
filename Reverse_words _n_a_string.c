90c60f66d93a3e05b1b6c71721f4345ea0ccabfb
pushkard999 <pushkard999@gmail.com>
2021-10-08 21:41:27 -0700

    Update Reverse_words _n_a_string.c

--- Reverse_words _n_a_string.c
+++ Reverse_words _n_a_string.c
@@ -26,7 +26,7 @@ int main()
 
     for(int i=0; i<str_len+1; i++) 
     {
-        if(*(ptr_name+i) == ' ' || *(ptr_name+i) =='\0' )
+        if(*(ptr_name+i) == ' ' || *(ptr_name+i) =='\0' )   //to get space and last null character from string
         {
             reverse_string(ptr_name+j, i-j);   //i-j gives length of the string if i=7, j=0 length=8
             j = i+1;    // to get length of next word
