89e956bfdda87d43156f2d2406bdeae13507bc30
pushkard999 <pushkard999@gmail.com>
2022-02-23 18:50:08 -0800

    Create even_odd_numbers.cpp

---
+++ even_odd_numbers.cpp
@@ -0,0 +1,14 @@
+/**     program for even and odd numbers  **/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num =0;
+    cin >> num;
+    
+    if(num%2 == 0)
+        cout << "even";
+    else
+        cout << "odd";
+    
+}
